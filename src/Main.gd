a6466ec6ad1c41dd3f3d694686adc31747159680
Happy-reindeer007 <114765327+Happy-reindeer007@users.noreply.github.com>
2024-12-30 15:35:36 +0100

    Adds lighting and player movement
    Also some work in progress for killing the mobs

--- src/Main.gd
+++ src/Main.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
-var new_mob = load("res://Scenes/Mob.tscn")
+var new_mob = preload("res://Scenes/Mob.tscn")
 var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$MobSpawnerTimer.start()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,10 +13,9 @@ func _process(delta: float) -> void:
 	pass
 
 
-
 func _on_mob_spawner_timer_timeout() -> void:
 	# Randomizes the time it takes for a mob to spawn
-	var randfloat = rng.randf_range(3.0, 5.0)
+	var randfloat = rng.randf_range(3.0, 9.0)
 	$MobSpawnerTimer.wait_time = randfloat
 	
 	# Creates an instance of the mob and sets position
@@ -30,3 +29,8 @@ func _on_mob_spawner_timer_timeout() -> void:
 	
 	# Add child
 	add_child(mob_instance)
+	$MobSpawnerTimer.start()
+
+
+func _on_player_attack() -> void:
+	pass # Replace with function body.
--- src/player.gd
+++ src/player.gd
@@ -1,12 +1,18 @@
 extends Area2D
-var velocity = Vector2.ZERO
+
+signal attack
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$AnimatedSprite2D.play("walk")
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.x -= 5 * delta
-	pass
+	position.x += 0.05
+	if Input.is_action_pressed("kill_ghost"):
+		if $KillCooldown.time_left == 0:
+			$KillCooldown.start()
+			pass
+	
