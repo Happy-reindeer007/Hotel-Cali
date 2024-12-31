071cea75c56d9003472c2af2a78cd66c381ee088
Happy-reindeer007 <114765327+Happy-reindeer007@users.noreply.github.com>
2024-12-31 18:25:39 +0100

    Detects when mobs enter light area

--- src/player.gd
+++ src/player.gd
@@ -1,12 +1,10 @@
 extends Area2D
-
+var mob_killable
 signal attack
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$AnimatedSprite2D.play("walk")
-
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -16,3 +14,13 @@ func _process(delta: float) -> void:
 			$KillCooldown.start()
 			pass
 	
+
+
+func _on_lightarea_area_entered(area: Area2D) -> void:
+	if area != self:
+		mob_killable = true
+		print(mob_killable)
+
+func _on_lightarea_area_exited(area: Area2D) -> void:
+	mob_killable = false
+	print(mob_killable)
