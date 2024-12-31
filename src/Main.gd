7109b9fcbd923219d11b796bd8cc207cced1b062
Happy-reindeer007 <114765327+Happy-reindeer007@users.noreply.github.com>
2024-12-31 21:28:54 +0100

    Bug fix: mobs stop spawning when you reach the end

--- src/Main.gd
+++ src/Main.gd
@@ -15,21 +15,22 @@ func _process(delta: float) -> void:
 
 func _on_mob_spawner_timer_timeout() -> void:
 	# Randomizes the time it takes for a mob to spawn
-	var randfloat = rng.randf_range(3.0, 9.0)
-	$MobSpawnerTimer.wait_time = randfloat
-	
-	# Creates an instance of the mob and sets position
-	var mob_instance = new_mob.instantiate()
-	mob_instance.position = Vector2(180, 90)
-	
-	# Picks a random skin/animation
-	var mob_skins = mob_instance.get_node("AnimatedSprite2D").sprite_frames.get_animation_names()
-	var randint = rng.randi_range(0, 2)
-	mob_instance.get_node("AnimatedSprite2D").play(mob_skins[randint])
-	
-	# Add child
-	add_child(mob_instance)
-	$MobSpawnerTimer.start()
+	if $Player.position.x < 50: 
+		var randfloat = rng.randf_range(3.0, 9.0)
+		$MobSpawnerTimer.wait_time = randfloat
+		
+		# Creates an instance of the mob and sets position
+		var mob_instance = new_mob.instantiate()
+		mob_instance.position = Vector2(180, 90)
+		
+		# Picks a random skin/animation
+		var mob_skins = mob_instance.get_node("AnimatedSprite2D").sprite_frames.get_animation_names()
+		var randint = rng.randi_range(0, 2)
+		mob_instance.get_node("AnimatedSprite2D").play(mob_skins[randint])
+		
+		# Add child
+		add_child(mob_instance)
+		$MobSpawnerTimer.start()
 
 
 func _on_player_hit(area: Area2D) -> void:
